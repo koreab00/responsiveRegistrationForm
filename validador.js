79afbe7a16f6af067b652fa4522201e5f86ae9fa
koreab00 <pauloalhadef4@gmail.com>
2022-09-15 12:00:54 -0300

    checagem de cep com API

--- validador.js
+++ validador.js
@@ -28,32 +28,68 @@ lenghtCel.addEventListener('keypress' , () =>{
 	}
 })
 
-//CEP//
+// Endereço
 
-let cep = document.querySelector('#cep')
-let rua = document.querySelector('#rua')
-let bairro = document.querySelector('#bairro')
-let cidade = document.querySelector('#cidade')
-let estado = document.querySelector('#estado')
+const addresForm = document.querySelector("#address-form")
+const cepInput = document.querySelector("#cep")
+const addressInput = document.querySelector("#address")
+const cityInput = document.querySelector("#city")
+const neighborhoodInput = document.querySelector("#neighborhood")
+const regionInput = document.querySelector("#region")
+const formInputs = document.querySelectorAll("[data-input]")
 
-cep.value = '01001000';
+const closeButton = document.querySelector("#close-message")
 
-cep.addEventListener('blur', function(e) {
-	let cep = e.target.value;
-	let script = document.createElement('script');
-	script.src = 'https://viacep.com.br/ws/'+cep+'/json/?callback=popularForm';
-	document.body.appendChild(script);
-});
+cepInput.addEventListener("keypress", (e) =>{
+	const onlyNumbers = /[0-9]/;
+	const key = String.fromCharCode(e.keyCode);
 
-function popularForm(resposta) {
-	if("erro" in resposta) {
-		alert('CEP não encontrado');
+	// allow only numbers
+	if(!onlyNumbers.test(key)) {
+		e.preventDefault();
 		return;
 	}
+});
 
-console.log(resposta);
-rua.value = reposta.logradouro;
-bairro.value = resposta.bairro;
-cidade.value = resposta.localidade;
-estado.value = resposta.uf;
+// Get address event
+
+cepInput.addEventListener("keyup" ,(e) =>{
+	const inputValue = e.target.value
+
+	// Check if we have the correct length
+	if(inputValue.length === 8) {
+		getAddress(inputValue);
+	}
+});
+
+// Get customer address from API
+
+const getAddress = async (cep) => {
+	toggleLoader();
+
+	cepInput.blur();
+
+	const apiUrl = `https://viacep.com.br/ws/${cep}/json/`;
+
+	const response = await fetch(apiUrl);
+
+	const data = await response.json();
+
+	// Show error and reset form
+	if(data.erro === "true") {
+		addresForm.reset();
+		toggleLoader();
+		//Show message
+		return;
+	}
+};
+
+// Show or hide loader
+
+const toggleLoader = () => {
+	const fadeElement = document.querySelector("#fade");
+	const loaderElemnt = document.querySelector("#loader");
+
+	fadeElement.classList.toggle("hide");
+	loaderElemnt.classList.toggle("hide");
 }
